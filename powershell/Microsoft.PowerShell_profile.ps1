628da330cdfa78a982077e6815a7b6ce1dd10057
Alex Kurtz <kurtzalexo@gmail.com>
2026-03-14 20:36:29 +0200

    init

---
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,45 @@
+Import-Module -Name Terminal-Icons
+
+# Set utf-8 for input/output 
+[Console]::InputEncoding = [System.Text.Encoding]::UTF8
+[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+
+# Set utf-8 for all commands 
+$OutputEncoding = [System.Text.Encoding]::UTF8
+
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
+
+$env:FZF_DEFAULT_COMMAND = 'fd --type f --strip-cwd-prefix --hidden --exclude .git'
+
+function fzf-open {
+    param(
+        [Parameter(Mandatory=$false)]
+        [string]$Editor
+    )
+
+    # Запускаем fzf и сохраняем выбор
+    $selection = fzf --height 40% --reverse --border
+
+    # Проверяем, что файл был выбран (не нажали Esc)
+    if ($selection) {
+        if ($Editor) {
+            # Если указана программа (например, nvim), запускаем её с файлом
+            # & -- это оператор вызова в PowerShell
+            & $Editor $selection
+        } else {
+            # Если программа не указана, открываем стандартным средством Windows
+            Start-Process $selection
+        }
+    }
+}
+
+Set-Alias fo fzf-open
+
+fnm env --use-on-cd --version-file-strategy=recursive --shell powershell | Out-String | Invoke-Expression
+
+Set-Alias g git
+Set-Alias ll Get-ChildItem
+
+function snip {
+    & "C:\Users\alex\Scripts\Snippets\Get-Snippet.ps1"
+}
